f6c1baf2e3819af4bce95cbaf7064b35d0dfd8d2
MaxKoval1ov <maxkov112211@gmail.com>
2022-03-02 20:19:20 +0300

    feat:sec change

--- new-project/src/app/app.component.ts
+++ new-project/src/app/app.component.ts
@@ -20,8 +20,6 @@ export class AppComponent {
     const higherOrder = clicks.pipe(map((ev) => interval(1000)));
     const firstOrder = higherOrder.pipe(mergeAll());
     firstOrder.subscribe(x => console.log(x));
-    
-    
   }
 
 
